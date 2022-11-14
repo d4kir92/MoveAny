50a0ad7fe74b839cf99424fd130224147c7d3441
D4KiR <d4kir92@gmail.com>
2022-11-14 19:49:44 +0100

    v0.8.24

--- malock.lua
+++ malock.lua
@@ -2,7 +2,7 @@
 local AddOnName, MoveAny = ...
 
 local config = {
-	["title"] = format( "MoveAny |T135994:16:16:0:0|t v|cff3FC7EB%s", "0.8.23" )
+	["title"] = format( "MoveAny |T135994:16:16:0:0|t v|cff3FC7EB%s", "0.8.24" )
 }
 
 local PREFIX = "MOAN"
