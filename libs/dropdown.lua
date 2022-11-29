c908217509e9fdd48b46580b4ff7bc61741a8a70
D4KiR <d4kir92@gmail.com>
2022-11-29 08:07:39 +0100

    v0.9.26

--- libs/dropdown.lua
+++ libs/dropdown.lua
@@ -1,10 +1,10 @@
 
 local AddOnName, MoveAny = ...
 
-local LibDD = LibStub:GetLibrary("LibUIDropDownMenu-4.0")
+--local LibDD = LibStub:GetLibrary("LibUIDropDownMenu-4.0")
 
 function MoveAny:CreateDropdown( opts )
-	local CB = LibDD:Create_UIDropDownMenu( opts.name, opts.parent )
+	--[[local CB = LibDD:Create_UIDropDownMenu( opts.name, opts.parent )
 	LibDD:UIDropDownMenu_SetWidth( CB, 120 )
 	LibDD:UIDropDownMenu_SetText( CB, opts.defaultVal )
 	LibDD:UIDropDownMenu_Initialize( CB, function( self, level, menuList )
@@ -27,7 +27,31 @@ function MoveAny:CreateDropdown( opts )
 	local text = CB:CreateFontString(nil, "ARTWORK")
 	text:SetFont(STANDARD_TEXT_FONT, 12, "THINOUTLINE")
 	text:SetPoint("LEFT", CB, "RIGHT", 0, 4)
+	text:SetText( ImproveAny:GT(opts.title) )
+
+    return CB]]
+
+	local Menu = LibStub("LibDropDown"):NewButton(opts.parent, "MyMenuButton")
+	Menu:SetPoint("TOPLEFT", 0, -4)
+	Menu:SetJustifyH("LEFT")
+	Menu:SetStyle("DEFAULT")
+	Menu:SetText( opts.defaultVal )
+
+	for i, v in pairs( opts.items ) do
+		Menu:Add({
+			text = v,
+			args = {i, v},
+			func = function(self, button, key, value)
+				opts.changeFunc( Menu, value )
+				Menu:SetText( value )
+			end
+		})
+	end
+
+	local text = Menu:CreateFontString(nil, "ARTWORK")
+	text:SetFont(STANDARD_TEXT_FONT, 12, "THINOUTLINE")
+	text:SetPoint("LEFT", Menu, "RIGHT", 0, 4)
 	text:SetText( MoveAny:GT(opts.title) )
 
-    return CB
+	return Menu
 end
