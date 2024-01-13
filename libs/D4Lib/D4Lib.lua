7d53b0fe18314eb3315c10905d0fcf8feaa42c54
D4KiR <23246564+d4kir92@users.noreply.github.com>
2024-01-13 11:06:37 +0100

    v1.6.113

--- libs/D4Lib/D4Lib.lua
+++ libs/D4Lib/D4Lib.lua
@@ -20,6 +20,46 @@ function D4:GetWoWBuild()
 end
 
 --[[ QOL ]]
+local ICON_TAG_LIST_EN = {
+    ["star"] = 1,
+    ["yellow"] = 1,
+    ["cirlce"] = 2,
+    ["orange"] = 2,
+    ["diamond"] = 3,
+    ["triangle"] = 4,
+    ["moon"] = 5,
+    ["square"] = 6,
+    ["blue"] = 6,
+    ["cross"] = 7,
+    ["red"] = 7,
+    ["skull"] = 8,
+}
+
+local function FixIconChat(self, event, message, author, ...)
+    if ICON_LIST then
+        for tag in string.gmatch(message, "%b{}") do
+            local term = strlower(string.gsub(tag, "[{}]", ""))
+            if ICON_TAG_LIST_EN[term] and ICON_LIST[ICON_TAG_LIST_EN[term]] then
+                message = string.gsub(message, tag, ICON_LIST[ICON_TAG_LIST_EN[term]] .. "0|t")
+            end
+        end
+    end
+
+    return false, message, author, ...
+end
+
+local chatChannels = {}
+for i, v in pairs(_G) do
+    if string.find(i, "CHAT_MSG_", 1, true) and not tContains(chatChannels, i) then
+        tinsert(chatChannels, i)
+    end
+end
+
+for i, v in pairs(chatChannels) do
+    ChatFrame_AddMessageEventFilter(i, FixIconChat)
+end
+
+ChatFrame_AddMessageEventFilter("CHAT_MSG_CHANNEL", FixIconChat)
 C_Timer.After(
     4,
     function()
