549e3f8d0e853d3ffa509644e63f112a6d81bee5
D4KiR <23246564+d4kir92@users.noreply.github.com>
2024-01-19 08:51:33 +0100

    v1.6.118

--- core.lua
+++ core.lua
@@ -23,17 +23,6 @@ MAHIDDEN.auraRows = 0
 --[[ NEW ]]
 local MAUIP = CreateFrame("Frame", "UIParent")
 MAUIP:SetAllPoints(UIParent)
-C_Timer.After(
-	0,
-	function()
-		if SunnArt_DummyFrame then
-			MAUIP:SetAllPoints(SunnArt_DummyFrame)
-		else
-			MAUIP:SetAllPoints(UIParent)
-		end
-	end
-)
-
 MAUIP.unit = "player"
 MAUIP.auraRows = 0
 hooksecurefunc(
