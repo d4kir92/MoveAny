f3c991cab486a4e186c68e84819847182f957ef8
D4KiR <23246564+d4kir92@users.noreply.github.com>
2024-02-07 07:44:36 +0100

    v1.6.123

--- settings.lua
+++ settings.lua
@@ -373,8 +373,8 @@ function MoveAny:InitMALock()
 		end
 	)
 
-	D4:SetVersion(AddonName, 135994, "1.6.122")
-	MALock.TitleText:SetText(format("MoveAny |T135994:16:16:0:0|t v|cff3FC7EB%s", "1.6.122"))
+	D4:SetVersion(AddonName, 135994, "1.6.123")
+	MALock.TitleText:SetText(format("MoveAny |T135994:16:16:0:0|t v|cff3FC7EB%s", "1.6.123"))
 	MALock.CloseButton:SetScript(
 		"OnClick",
 		function()
@@ -979,7 +979,7 @@ function MoveAny:ShowProfiles()
 			end
 		)
 
-		MAProfiles.TitleText:SetText(format("MoveAny |T135994:16:16:0:0|t v|cff3FC7EB%s", "1.6.122"))
+		MAProfiles.TitleText:SetText(format("MoveAny |T135994:16:16:0:0|t v|cff3FC7EB%s", "1.6.123"))
 		MAProfiles.CloseButton:SetScript(
 			"OnClick",
 			function()
