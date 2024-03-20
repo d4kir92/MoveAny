4f46be6014861db4c234bb7fb09d733cbc1a0011
D4KiR <23246564+d4kir92@users.noreply.github.com>
2024-03-20 19:25:10 +0100

    v1.6.137

--- ele/minimap.lua
+++ ele/minimap.lua
@@ -1,9 +1,7 @@
 local _, MoveAny = ...
-
 function MoveAny:InitMinimap()
 	if not MoveAny:IsEnabled("MINIMAP", false) then return end
 	if ElvUI then return end
-
 	if MinimapToggleButton then
 		MinimapToggleButton:SetParent(MAHIDDEN)
 	end
@@ -12,45 +10,52 @@ function MoveAny:InitMinimap()
 		MinimapBorderTop:SetParent(MAHIDDEN)
 	end
 
-	hooksecurefunc(Minimap, "SetAlpha", function()
-		local alpha = MoveAny:GetMainPanel():GetAlpha()
-
-		if alpha == 0 then
-			if self.ma_setalpha then return end
-			self.ma_setalpha = true
-			Minimap:SetAlpha(0)
-			self.ma_setalpha = false
-		end
-	end)
-
-	if MinimapBackdrop then
-		hooksecurefunc(MinimapBackdrop, "SetAlpha", function()
+	hooksecurefunc(
+		Minimap,
+		"SetAlpha",
+		function()
 			local alpha = MoveAny:GetMainPanel():GetAlpha()
-
 			if alpha == 0 then
 				if self.ma_setalpha then return end
 				self.ma_setalpha = true
-				MinimapBackdrop:SetAlpha(0)
+				Minimap:SetAlpha(0)
 				self.ma_setalpha = false
 			end
-		end)
+		end
+	)
+
+	if MinimapBackdrop then
+		hooksecurefunc(
+			MinimapBackdrop,
+			"SetAlpha",
+			function()
+				local alpha = MoveAny:GetMainPanel():GetAlpha()
+				if alpha == 0 then
+					if self.ma_setalpha then return end
+					self.ma_setalpha = true
+					MinimapBackdrop:SetAlpha(0)
+					self.ma_setalpha = false
+				end
+			end
+		)
 	end
 
 	local debugMinimap = false
-
-	local miniMapTab = {"MiniMapMailFrame", "GarrisonLandingPageMinimapButton", "QueueStatusMinimapButton", "MiniMapInstanceDifficulty", "MiniMapChallengeMode", "MiniMapLFGFrame", "MiniMapBattlefieldFrame", "MiniMapTracking"}
-
+	local miniMapTab = {"MiniMapMailFrame", "GarrisonLandingPageMinimapButton", "QueueStatusMinimapButton", "MiniMapInstanceDifficulty", "MiniMapChallengeMode", "MiniMapLFGFrame", "MiniMapBattlefieldFrame", "MiniMapTracking", "HelpOpenWebTicketButton"}
 	if debugMinimap then
 		for i, name in pairs(miniMapTab) do
 			local btn = _G[name]
-
 			if btn then
-				hooksecurefunc(btn, "Hide", function(sel)
-					if sel.mashow then return end
-					sel.mashow = true
-					sel:Show()
-					sel.mashow = false
-				end)
+				hooksecurefunc(
+					btn,
+					"Hide",
+					function(sel)
+						if sel.mashow then return end
+						sel.mashow = true
+						sel:Show()
+						sel.mashow = false
+					end
+				)
 
 				btn:Show()
 			else
