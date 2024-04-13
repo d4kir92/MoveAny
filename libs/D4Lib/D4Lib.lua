fe89cd7ed5c7de807514b6f4578498baa049ce3c
D4KiR <23246564+d4kir92@users.noreply.github.com>
2024-04-13 13:13:46 +0200

    v1.6.150

--- libs/D4Lib/D4Lib.lua
+++ libs/D4Lib/D4Lib.lua
@@ -104,3 +104,122 @@ for i, v in pairs(chatChannels) do
 end
 
 ChatFrame_AddMessageEventFilter("CHAT_MSG_CHANNEL", FixIconChat)
+if D4:GetWoWBuild() == "CLASSIC" then
+    -- FIX HEALTH
+    TargetFrameTextureFrame:CreateFontString("TargetFrameHealthBarText", "BORDER", "TextStatusBarText")
+    TargetFrameHealthBarText:SetPoint("CENTER", TargetFrameTextureFrame, "CENTER", -50, 3)
+    TargetFrameTextureFrame:CreateFontString("TargetFrameHealthBarTextLeft", "BORDER", "TextStatusBarText")
+    TargetFrameHealthBarTextLeft:SetPoint("LEFT", TargetFrameTextureFrame, "LEFT", 8, 3)
+    TargetFrameTextureFrame:CreateFontString("TargetFrameHealthBarTextRight", "BORDER", "TextStatusBarText")
+    TargetFrameHealthBarTextRight:SetPoint("RIGHT", TargetFrameTextureFrame, "RIGHT", -110, 3)
+    TargetFrameTextureFrame:CreateFontString("TargetFrameManaBarText", "BORDER", "TextStatusBarText")
+    TargetFrameManaBarText:SetPoint("CENTER", TargetFrameTextureFrame, "CENTER", -50, -8)
+    TargetFrameTextureFrame:CreateFontString("TargetFrameManaBarTextLeft", "BORDER", "TextStatusBarText")
+    TargetFrameManaBarTextLeft:SetPoint("LEFT", TargetFrameTextureFrame, "LEFT", 8, -8)
+    TargetFrameTextureFrame:CreateFontString("TargetFrameManaBarTextRight", "BORDER", "TextStatusBarText")
+    TargetFrameManaBarTextRight:SetPoint("RIGHT", TargetFrameTextureFrame, "RIGHT", -110, -8)
+    TargetFrameHealthBar.LeftText = TargetFrameHealthBarTextLeft
+    TargetFrameHealthBar.RightText = TargetFrameHealthBarTextRight
+    TargetFrameManaBar.LeftText = TargetFrameManaBarTextLeft
+    TargetFrameManaBar.RightText = TargetFrameManaBarTextRight
+    UnitFrameHealthBar_Initialize("target", TargetFrameHealthBar, TargetFrameHealthBarText, true)
+    UnitFrameManaBar_Initialize("target", TargetFrameManaBar, TargetFrameManaBarText, true)
+    if FocusFrame then
+        UnitFrameHealthBar_Initialize("focus", FocusFrameHealthBar, FocusFrameHealthBarText, true)
+        UnitFrameManaBar_Initialize("focus", FocusFrameManaBar, FocusFrameManaBarText, true)
+    end
+
+    local function TextStatusBar_UpdateTextStringWithValues(statusFrame, textString, value, valueMin, valueMax)
+        if statusFrame.LeftText and statusFrame.RightText then
+            statusFrame.LeftText:SetText("")
+            statusFrame.RightText:SetText("")
+            statusFrame.LeftText:Hide()
+            statusFrame.RightText:Hide()
+        end
+
+        if (tonumber(valueMax) ~= valueMax or valueMax > 0) and not statusFrame.pauseUpdates then
+            statusFrame:Show()
+            if (statusFrame.cvar and GetCVar(statusFrame.cvar) == "1" and statusFrame.textLockable) or statusFrame.forceShow then
+                textString:Show()
+            elseif statusFrame.lockShow > 0 and (not statusFrame.forceHideText) then
+                textString:Show()
+            else
+                textString:SetText("")
+                textString:Hide()
+
+                return
+            end
+
+            if value == 0 and statusFrame.zeroText then
+                textString:SetText(statusFrame.zeroText)
+                statusFrame.isZero = 1
+                textString:Show()
+
+                return
+            end
+
+            statusFrame.isZero = nil
+            local valueDisplay = value
+            local valueMaxDisplay = valueMax
+            if statusFrame.numericDisplayTransformFunc then
+                valueDisplay, valueMaxDisplay = statusFrame.numericDisplayTransformFunc(value, valueMax)
+            else
+                valueDisplay = AbbreviateLargeNumbers(value)
+                valueMaxDisplay = AbbreviateLargeNumbers(valueMax)
+            end
+
+            local shouldUsePrefix = statusFrame.prefix and (statusFrame.alwaysPrefix or not (statusFrame.cvar and GetCVar(statusFrame.cvar) == "1" and statusFrame.textLockable))
+            local displayMode = GetCVar("statusTextDisplay")
+            if statusFrame.showNumeric then
+                displayMode = "NUMERIC"
+            end
+
+            if statusFrame.disablePercentages and displayMode == "PERCENT" then
+                displayMode = "NUMERIC"
+            end
+
+            if valueMax <= 0 or displayMode == "NUMERIC" or displayMode == "NONE" then
+                if shouldUsePrefix then
+                    textString:SetText(statusFrame.prefix .. " " .. valueDisplay .. " / " .. valueMaxDisplay)
+                else
+                    textString:SetText(valueDisplay .. " / " .. valueMaxDisplay)
+                end
+            elseif displayMode == "BOTH" then
+                if statusFrame.LeftText and statusFrame.RightText then
+                    if not statusFrame.disablePercentages and (not statusFrame.powerToken or statusFrame.powerToken == "MANA") then
+                        statusFrame.LeftText:SetText(math.ceil((value / valueMax) * 100) .. "%")
+                        statusFrame.LeftText:Show()
+                    end
+
+                    statusFrame.RightText:SetText(valueDisplay)
+                    statusFrame.RightText:Show()
+                    textString:Hide()
+                else
+                    valueDisplay = valueDisplay .. " / " .. valueMaxDisplay
+                    if not statusFrame.disablePercentages then
+                        valueDisplay = "(" .. math.ceil((value / valueMax) * 100) .. "%) " .. valueDisplay
+                    end
+                end
+
+                textString:SetText(valueDisplay)
+            elseif displayMode == "PERCENT" then
+                valueDisplay = math.ceil((value / valueMax) * 100) .. "%"
+                if shouldUsePrefix then
+                    textString:SetText(statusFrame.prefix .. " " .. valueDisplay)
+                else
+                    textString:SetText(valueDisplay)
+                end
+            end
+        else
+            textString:Hide()
+            textString:SetText("")
+            if not statusFrame.alwaysShow then
+                statusFrame:Hide()
+            else
+                statusFrame:SetValue(0)
+            end
+        end
+    end
+
+    hooksecurefunc("TextStatusBar_UpdateTextStringWithValues", TextStatusBar_UpdateTextStringWithValues)
+end
