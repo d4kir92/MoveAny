560f24c762285c9131d1c3486179f67bfb61516e
D4KiR <23246564+d4kir92@users.noreply.github.com>
2024-09-09 13:14:47 +0200

    v1.7.16

--- settings.lua
+++ settings.lua
@@ -1,9 +1,4 @@
 local AddonName, MoveAny = ...
-local MAMMBTN = nil
-function MoveAny:GetMinimapButton()
-	return MAMMBTN
-end
-
 local PREFIX = "MOAN"
 local MASendProfiles = {}
 local MAWantProfiles = {}
@@ -469,8 +464,8 @@ function MoveAny:InitMALock()
 		end
 	)
 
-	MoveAny:SetVersion(AddonName, 135994, "1.7.15")
-	MALock.TitleText:SetText(format("MoveAny |T135994:16:16:0:0|t v|cff3FC7EB%s", "1.7.15"))
+	MoveAny:SetVersion(AddonName, 135994, "1.7.16")
+	MALock.TitleText:SetText(format("MoveAny |T135994:16:16:0:0|t v|cff3FC7EB%s", "1.7.16"))
 	MALock.CloseButton:SetScript(
 		"OnClick",
 		function()
@@ -1080,7 +1075,7 @@ function MoveAny:ShowProfiles()
 			end
 		)
 
-		MAProfiles.TitleText:SetText(format("MoveAny |T135994:16:16:0:0|t v|cff3FC7EB%s", "1.7.15"))
+		MAProfiles.TitleText:SetText(format("MoveAny |T135994:16:16:0:0|t v|cff3FC7EB%s", "1.7.16"))
 		MAProfiles.CloseButton:SetScript(
 			"OnClick",
 			function()
@@ -4786,8 +4781,8 @@ function MoveAny:LoadAddon()
 					{
 						["name"] = "MoveAny",
 						["icon"] = 135994,
-						["dbtab"] = CVTAB,
-						["vTT"] = {{"MoveAny |T135994:16:16:0:0|t", "v|cff3FC7EB1.7.15"}, {MoveAny:GT("LID_LEFTCLICK"), MoveAny:GT("LID_MMBTNLEFT")}, {MoveAny:GT("LID_RIGHTCLICK"), MoveAny:GT("LID_MMBTNRIGHT")}},
+						["dbtab"] = MATAB,
+						["vTT"] = {{"MoveAny |T135994:16:16:0:0|t", "v|cff3FC7EB1.7.16"}, {MoveAny:GT("LID_LEFTCLICK"), MoveAny:GT("LID_MMBTNLEFT")}, {MoveAny:GT("LID_RIGHTCLICK"), MoveAny:GT("LID_MMBTNRIGHT")}},
 						["funcL"] = function()
 							MoveAny:ToggleMALock()
 						end,
