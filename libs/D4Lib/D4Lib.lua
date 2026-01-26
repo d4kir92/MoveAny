c92ee8dab313b33ef2607db16695c12d8a29601a
D4KiR <23246564+d4kir92@users.noreply.github.com>
2026-01-26 17:40:25 +0100

    v1.8.245

--- libs/D4Lib/D4Lib.lua
+++ libs/D4Lib/D4Lib.lua
@@ -58,6 +58,33 @@ if getglobal("C_Timer") == nil then
     D4.oldWow = true
 end
 
+local f = CreateFrame("Frame")
+f.tab = {}
+f:Hide()
+f:SetScript(
+    "OnUpdate",
+    function(self, elapsed)
+        local currentTime = GetTime()
+        for i = #self.tab, 1, -1 do
+            local data = self.tab[i]
+            if data[1] <= currentTime then
+                local func = data[2]
+                table.remove(self.tab, i)
+                func()
+            end
+        end
+
+        if #self.tab == 0 then
+            self:Hide()
+        end
+    end
+)
+
+function D4:ExtraAfter(duration, callback, from)
+    table.insert(f.tab, {GetTime() + duration, callback, from})
+    f:Show()
+end
+
 if getglobal("C_Widget") == nil then
     setglobal("C_Widget", {})
     function C_Widget.IsWidget(frame)
@@ -73,6 +100,17 @@ local debug = false
 function D4:SetDebug(bo)
     debug = bo
 end
+
+local ready = false
+local test = CreateFrame("frame")
+test:RegisterEvent("PLAYER_ENTERING_WORLD")
+test:SetScript(
+    "OnEvent",
+    function(...)
+        ready = true
+        test:UnregisterEvent("PLAYER_ENTERING_WORLD")
+    end
+)
 
 function D4:After(time, callback, from)
     if from == nil then
@@ -90,6 +128,17 @@ function D4:After(time, callback, from)
     if debug then
         countAfter[from] = countAfter[from] or 0
         countAfter[from] = countAfter[from] + 1
+    end
+
+    if not ready then
+        D4:ExtraAfter(
+            time,
+            function()
+                callback()
+            end, from
+        )
+
+        return
     end
 
     C_Timer.After(
