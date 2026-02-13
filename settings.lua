f5fa6df509ec3466d13a26c028bf5b11de83ea66
D4KiR <23246564+d4kir92@users.noreply.github.com>
2026-02-13 02:04:07 +0100

    v1.8.250

--- settings.lua
+++ settings.lua
@@ -2464,7 +2464,7 @@ function MoveAny:PlayerLogin()
 		end
 	end
 
-	MoveAny:SetVersion(135994, "1.8.249")
+	MoveAny:SetVersion(135994, "1.8.250")
 	if MoveAny.GetVersion ~= nil and MoveAny:GetVersion() ~= nil and MoveAny.Trans ~= nil then
 		MoveAny:CreateMinimapButton(
 			{
