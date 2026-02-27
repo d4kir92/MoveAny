1fc5bdae9d1d6321260027e3a5eb1d9485f6f590
D4KiR <23246564+d4kir92@users.noreply.github.com>
2026-02-27 01:06:59 +0100

    v1.8.253

--- settings.lua
+++ settings.lua
@@ -2464,7 +2464,7 @@ function MoveAny:PlayerLogin()
 		end
 	end
 
-	MoveAny:SetVersion(135994, "1.8.252")
+	MoveAny:SetVersion(135994, "1.8.253")
 	if MoveAny.GetVersion ~= nil and MoveAny:GetVersion() ~= nil and MoveAny.Trans ~= nil then
 		MoveAny:CreateMinimapButton(
 			{
@@ -6319,10 +6319,14 @@ function MoveAny:LoadAddon()
 
 				function MoveAny:ThinkTooltip()
 					if GameTooltip:IsShown() then
-						local p1, p2, p3, p4, p5 = GameTooltip:GetPoint()
-						if p1 and p3 then
-							GameTooltip:SetPoint(p1, p2, p3, p4, p5)
-						end
+						pcall(
+							function()
+								local p1, p2, p3, p4, p5 = GameTooltip:GetPoint()
+								if p1 and p3 then
+									GameTooltip:SetPoint(p1, p2, p3, p4, p5)
+								end
+							end
+						)
 
 						MoveAny:After(
 							0.03,
