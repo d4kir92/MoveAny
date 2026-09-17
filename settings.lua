7f61b4a2e71ec3cd4c6e41b30f503441b154cada
D4KiR <23246564+d4kir92@users.noreply.github.com>
2026-09-17 10:33:18 +0200

    v1.12.6

--- settings.lua
+++ settings.lua
@@ -1692,7 +1692,7 @@ function MoveAny:PlayerLogin()
 		return MoveAny:Trans("LID_LOCKWINDOWS")
 	end
 
-	MoveAny:SetVersion(135994, "1.12.5")
+	MoveAny:SetVersion(135994, "1.12.6")
 	if MoveAny.GetVersion ~= nil and MoveAny:GetVersion() ~= nil and MoveAny.Trans ~= nil then
 		MoveAny:CreateMinimapButton({
 			["name"] = "MoveAny",
